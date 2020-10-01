3f1fb240cefac00ff53a74b6537306f13a81afd9
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-01 02:04:27 +0100

    Style buttons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>pomodoro</h2>
+      <h2>pomodoro!</h2>
       <div className='timer'>
         <span>00</span>
         <span>:</span>
