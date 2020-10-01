cf853ec627bce0dac30375317113cf9cb9d0fa12
Lhoussaine <bouaik.lhou@gmail.com>
2020-10-01 01:43:54 +0100

    Make layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h2>pomodoro</h2>
+      <div className='timer'>
+        <span>00</span>
+        <span>:</span>
+        <span>00</span>
+      </div>
+      <div className='buttons'>
+        <button>start</button>
+        <button>stop</button>
+        <button>reset</button>
+      </div>
     </div>
   );
 }
